4a8d19c505e31628cbe588abf8944917e81ad173
Juan de Dios <juandedioz@gmail.com>
2023-10-27 15:54:41 -0500

    Initial Commit

---
+++ ButtonAnimation/ButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAnimationApp.swift
+//  ButtonAnimation
+//
+//  Created by Juan de Dios on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonAnimation
+//
+//  Created by Juan de Dios on 27/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
